c74550ca23f60c2777d74f5ed42f2e602bc259a8
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-09 18:55:08 -0800

    added hanoi steps

---
+++ mine/hanoi.py
@@ -0,0 +1,11 @@
+# Jacobus Burger (2022)
+# I'm not sure if this problem exists elsewhere (it probably does).
+# Basically, try to determine the number of steps of a tower of n disks
+
+
+# the minimal number of moves for hanoi is 2^n - 1
+# I solved that by realizing that 2 disks take 3 steps and
+#   3 disks take 7, which follow that pattern.
+class Solution:
+    def hanoi(disks):
+        return 2**disks - 1
