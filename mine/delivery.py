2215ece5efe0e02b784b59cae41f17e27acfc97a
Jacobus Burger <therealjacoburger@gmail.com>
2023-01-15 18:16:04 -0800

    added new problem idea

---
+++ mine/delivery.py
@@ -0,0 +1,10 @@
+# Jacobus Burger (2023)
+# Problem:
+#   You've been given a stack of coloured papers/towels that need to be
+#   delivered to color-coded recepticles that match their colour, but
+#   the pile is not in order, and you don't have time to sort the pile
+#   or revisit any bins on your linear path. Find a way to get all the
+#   items to their correct destination without revisiting!
+#   You can only add and remove items from the top of the pile.
+#   You cannot return to a previously visited bin.
+#   You must not have any leftovers when you reach the end of the run.
