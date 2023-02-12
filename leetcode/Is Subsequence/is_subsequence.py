483f465edab6b1c1c49add67f98c50fd41339d8e
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-11 19:30:06 -0800

    added reference solution

--- leetcode/Is Subsequence/is_subsequence.py
+++ leetcode/Is Subsequence/is_subsequence.py
@@ -6,3 +6,14 @@ class Solution:
         if len(s) > 0:
             return False
         return True
+
+
+    # alternatively, we can use two counters to compare the strings
+    #   as NeetCode demonstrated in [this vid](https://www.youtube.com/watch?v=99RVfqklbCE)
+    def sol2(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return True if i == len(s) else False
