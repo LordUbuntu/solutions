725e2bc0cb64935e396681d1f96690f17bf02d90
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-23 23:15:28 -0800

    added maximum subarray solution

---
+++ leetcode/Maximum Subarray/maximum_subarray.py
@@ -0,0 +1,12 @@
+# Maximum Subarray Problem
+#   from: https://leetcode.com/problems/maximum-subarray/description/
+# Kudane's Algorithm: https://www.youtube.com/watch?v=86CQq3pKSUw
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # utilizing Kudane's algorithm
+        current = maximum = nums[0]
+        for i in range(1, len(nums)):
+            current = max(nums[i], current + nums[i])
+            if current > maximum:
+                maximum = current
+        return maximum
