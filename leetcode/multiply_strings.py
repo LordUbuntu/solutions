bb4e565c154ac17f63854b74ed86da3902cb85fa
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-06 12:39:31 -0800

    added new solution

---
+++ leetcode/multiply_strings.py
@@ -0,0 +1,6 @@
+# Jacobus Burger (2023)
+# Multiply two strings together
+# see: https://leetcode.com/problems/multiply-strings/description/
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
