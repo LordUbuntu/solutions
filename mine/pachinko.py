b3abbb43d8167bc8a552ec9d070f4ff49e40f7ef
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-13 11:21:02 -0700

    added pachinko (binary heap, postorder DFS) problem

---
+++ mine/pachinko.py
@@ -0,0 +1,3 @@
+# Jacobus Burger (2023)
+# A programming problem I've formulated that requires postorder DFS.
+#   You're dropping balls down a pachinko machine and notice that they always move too the left-most open space at the bottom if it's not yet taken. Given this, figure out where the next ball will go in the pile (binary heap).
