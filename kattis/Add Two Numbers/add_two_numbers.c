78f58603f67b62d30c1c801d9922181b867f1f14
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-23 23:38:55 -0700

    added c version

---
+++ kattis/Add Two Numbers/add_two_numbers.c
@@ -0,0 +1,10 @@
+// Jacobus Burger (2023)
+// Name says it all...
+// See: https://open.kattis.com/problems/addtwonumbers
+#include <stdio.h>
+
+int main(void) {
+        long long int a, b;
+        scanf("%lld%lld", &a, &b);
+        printf("%lld\n", (a + b));
+}
