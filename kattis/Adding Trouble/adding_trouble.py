5ccba544600e33e741042117d10ead372228cd5b
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-12 14:03:36 -0700

    added python solution

---
+++ kattis/Adding Trouble/adding_trouble.py
@@ -0,0 +1,8 @@
+# Jacobus Burger (2023)
+# See:
+#   https://open.kattis.com/problems/addingtrouble
+A, B, C = map(int, input().split())
+if A + B == C:
+    print("correct!")
+else:
+    print("wrong!")
