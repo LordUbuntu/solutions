0dfb3aaa2fd6681939ca2f5533d251ecac7bda6b
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-08 22:05:31 -0700

    start on WIP, first attempt fails (why?)

---
+++ leetcode/add_two_numbers.py
@@ -0,0 +1,28 @@
+# WORK IN PROGRESS
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Solution
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        solution = ListNode()
+        sol = solution
+        a = l1
+        b = l2
+        while (a is not None) and (b is not None):
+            s = (a.val if a is not None else 0) + (b.val if b is not None else 0)
+            c = s // 10
+            s = s % 10
+
+            sol.val = sol.val + s
+            if (a.next is not None) and (b.next is not None):
+                sol.next = ListNode(val=c)
+
+                a = a.next
+                b = b.next
+                sol = sol.next
+        return solution
