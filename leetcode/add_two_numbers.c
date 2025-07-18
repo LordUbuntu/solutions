e5fc4dea40708ae34f538a5cbf55204d0c7046db
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-18 00:59:47 -0700

    using what i learned from implementing singly linked list in C (LordUbuntu/algorithms) to start solving this one

---
+++ leetcode/add_two_numbers.c
@@ -0,0 +1,11 @@
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+        struct ListNode* solution = (struct ListNode*) malloc(sizeof(struct ListNode));
+        struct ListNode** head = &solution;
+}
