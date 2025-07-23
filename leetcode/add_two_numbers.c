dbd23f11c8520e83bc79a3aa1c10c0d157b286d1
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-22 20:00:57 -0700

    potential solution...

--- leetcode/add_two_numbers.c
+++ leetcode/add_two_numbers.c
@@ -1,3 +1,8 @@
+// Jacobus Burger (2025) solution
+#include <stdio.h>
+#include <stdlib.h>
+
+
 // Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -7,5 +12,21 @@ struct ListNode {
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
         struct ListNode* solution = (struct ListNode*) malloc(sizeof(struct ListNode));
-        struct ListNode** head = &solution;
+        solution->val = 0;
+        solution->next = NULL;
+        struct ListNode* current = solution;
+        int carry = 0;
+        printf("(0) %i %i, (h) %i %i, c %i\n", solution->val, solution->next, current->val, current->next, carry);
+
+        while (l1 != NULL && l2 != NULL && carry != 0) {
+                printf("  l1 %i %i, l2 %i %i, c %i\n", l1->val, l1->next, l2->val, l2->next, carry);
+                int sum = l1->val + l2->val + carry;
+                carry = sum / 10;
+                struct ListNode* next = (struct ListNode*) malloc(sizeof(struct ListNode));
+                next->val = sum % 10;
+                next->next = NULL;
+                current->next = next;
+                current = current->next;
+        }
+        return result;
 }
