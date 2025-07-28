be042fd2fe49a60201c1ff6023264a329417a3f6
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-28 16:00:31 -0700

    second attempt

--- leetcode/longest_substring_without_repeating_charadcters.c
+++ leetcode/longest_substring_without_repeating_charadcters.c
@@ -1,23 +1,39 @@
 // Jacobus Burger (2025-07-25)
 // Solution to Longest Substring Without Repeating Characters on LeetCode
+#include <stdio.h>
+#include <string.h>
+
 
 int lengthOfLongestSubstring(char* s) {
-    // this can be solved with a sliding window technique
-    size_t l = 0, r = 0;
-    int len = 0;
-    while (l < strlen(s) && r < strlen(s)) {
-        // scan from l to r to ensure no duplicates encountered by r
-        for (int i = l; i < r; i++) {
-        //  if duplicate encountered, reset l to current r
-            if (s[i] == s[r]) {
-                l = r;
-                break;
-            }
+        // avoid all the checking work when the string is empty
+        if (strlen(s) == 0)
+                return 0;
+
+        // this can be solved with a sliding window technique
+        size_t l = 0, r = 1;
+        int len = 0;
+
+        // pwwke
+        while (s[r] != '\0') {
+                for (int i = l; i < r; i++) {
+                        for (int j = l; i < r; j++) {
+                                if (i == j)
+                                        continue;
+                                if (s[i] == s[j]) {
+                                }
+                        if (s[i] == s[r]) {
+                                len = r - l > len ? r - l : len;
+                                l++;
+                                break;
+                        }
+                }
+                r++;
         }
-        // increment len
-        len = (r - l) + 1 > len ? (r - l) + 1 : len;
-        // increment r
-        r++;
-    }
-    return len;
+        return len;
+}
+
+
+int main(void) {
+        char* s1 = "wwke";
+        printf("s1 %i\n", lengthOfLongestSubstring(s1));
 }
