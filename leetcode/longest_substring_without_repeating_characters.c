56abec7e47205aa20b4f0dfcee28166f2eeba5d5
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-29 12:33:22 -0700

    trying again from scratch

--- leetcode/longest_substring_without_repeating_characters.c
+++ leetcode/longest_substring_without_repeating_characters.c
@@ -5,32 +5,7 @@
 
 
 int lengthOfLongestSubstring(char* s) {
-        // avoid all the checking work when the string is empty
-        if (strlen(s) == 0)
-                return 0;
-
-        // this can be solved with a sliding window technique
-        size_t l = 0, r = 1;
-        int len = 0;
-
-        // pwwke
-        while (s[r] != '\0') {
-                // check for any repeptitions
-                for (int i = l; i <= r; i++) {
-                        for (int j = l; i <= r; j++) {
-                                if (i == j)
-                                        continue;
-                                // if repetition found, count length, slide window
-                                if (s[i] == s[j]) {
-                                        len = r - l > len ? r - l : len;
-                                        l++;
-                                        break;
-                                }
-                        }
-                }
-                r++;
-        }
-        return len;
+        return 0;
 }
 
 
